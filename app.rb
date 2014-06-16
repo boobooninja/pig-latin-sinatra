79f45c8449d07fb97303fb4e22a6a117b98ee741
Loren Hale <loren.hale@gmail.com>
2014-06-16 16:40:23 -0500

    translator now using gem properly

--- app.rb
+++ app.rb
@@ -1,11 +1,23 @@
 require 'sinatra'
+require 'pig_latin'
 
 set :bind, '0.0.0.0'
+
+get '/' do
+  erb :translate
+end
 
 get '/translate' do
   erb :translate
 end
 
 post '/translate' do
-  erb :result
+  phrase = params["post"]["phrase"]
+  if (phrase)
+    @translated = PigLatin.translate( phrase )
+    # @translated = phrase
+    erb :result
+  else
+    erb :translate
+  end
 end
